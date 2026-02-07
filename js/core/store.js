9337741cf03e3fd0c0019c86f0b051e1923dfcfd
Q-pote <rabit.rail.nya.sskt.gmmsvc@gmail.com>
2026-02-07 11:11:39 +0900

    Update store.js
    JSONの呼び出し様にJS修正

--- js/core/store.js
+++ js/core/store.js
@@ -1,10 +1,21 @@
-import { NEKO_CITY_DATA } from "https://storage.googleapis.com/nekobi-data-bucket/neko_data.json";
+// import { NEKO_CITY_DATA } from "https://storage.googleapis.com/nekobi-data-bucket/neko_data.json";
+
+let _data = null;
+
+export async function loadData() {
+  const res = await fetch(
+    "https://storage.googleapis.com/nekobi-data-bucket/neko_data.json"
+  );
+  _data = await res.json();
+}
+
 export const Store = {
-    get data() { return NEKO_CITY_DATA || {}; },
-    get(year) { 
-        const yData = this.data[year];
-        if (!yData) return null;
-        if (!yData.year) yData.year = year;
-        return yData;
-    }
+  get data() { return _data || {}; },
+
+  get(year) {
+    const yData = this.data[year];
+    if (!yData) return null;
+    if (!yData.year) yData.year = year;
+    return yData;
+  }
 };
